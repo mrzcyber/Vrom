8231afa496b345ac608bba868dc24e5e900b8476
mrzcyber <mrzcyber@gmail.com>
2026-05-21 17:21:03 +0700

    fix(system):delete csrf middleware accept

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,9 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-          $middleware->validateCsrfTokens(except: [
-        '*',
-    ]);
     
          $middleware->alias([
             'admin'=>AdminMiddleware::class
